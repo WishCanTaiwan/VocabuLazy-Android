c3647808f7fae0f2ef2c2bca1712e9bf6ef40ba1
allencheng07 <allencheng07@hotmail.com>
2016-09-28 19:59:47 +0800

    Add GA screen event to Cover

--- app/src/main/java/com/wishcan/www/vocabulazy/cover/fragment/CoverDialogFragment.java
+++ app/src/main/java/com/wishcan/www/vocabulazy/cover/fragment/CoverDialogFragment.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 
 import com.wishcan.www.vocabulazy.R;
 import com.wishcan.www.vocabulazy.cover.view.CoverDialogView;
+import com.wishcan.www.vocabulazy.ga.manager.GAManager;
 import com.wishcan.www.vocabulazy.ga.tags.GAScreenName;
 import com.wishcan.www.vocabulazy.search.view.SearchAddVocToNoteDialogView;
 import com.wishcan.www.vocabulazy.widget.DialogFragmentNew;
@@ -19,45 +20,62 @@ import com.wishcan.www.vocabulazy.widget.DialogViewNew;
  */
 public class CoverDialogFragment extends DialogFragmentNew implements DialogViewNew.OnYesOrNoClickListener {
 
-    @Override
-    protected String getGALabel() {
-        return GAScreenName.TTS_ENGINE_INSTALL;
-    }
-
+    // callback interface
     public interface OnDialogClickListener {
         void onYesClicked();
         void onNoClicked();
     }
 
-    public static final String TAG = "C.DIALOG";
+    // TAG for debugging
+    public static final String TAG = "CoverDialogFragment";
 
+    // layout resource id
     private static final int LAYOUT_RES_ID = R.layout.view_cover_dialog;
 
+    // views
     private CoverDialogView mCoverDialogView;
     private OnDialogClickListener mOnDialogClickListener;
+
+    /** Life cycles **/
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+        // inflate cover dialog view
         if (mCoverDialogView == null) {
             mCoverDialogView = (CoverDialogView) inflater.inflate(LAYOUT_RES_ID, container, false);
+            mCoverDialogView.setOnYesOrNoClickListener(this);
         }
-        mCoverDialogView.setOnYesOrNoClickListener(this);
 
         return mCoverDialogView;
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+
+        // send GA screen event
+        GAManager.getInstance().sendScreenEvent(GAScreenName.TTS_ENGINE_INSTALL);
+    }
+
+    /** Abstracts and Interfaces **/
+
+    @Override
     public void onYesClick() {
-        Log.d(TAG, "yes");
         mOnDialogClickListener.onYesClicked();
     }
 
     @Override
     public void onNoClick() {
-        Log.d(TAG, "no");
         mOnDialogClickListener.onNoClicked();
     }
+
+    @Override
+    protected String getGALabel() {
+        return GAScreenName.TTS_ENGINE_INSTALL;
+    }
+
+    /** Public methods **/
 
     public void addOnDialogClickListener(OnDialogClickListener listener) {
         mOnDialogClickListener = listener;
