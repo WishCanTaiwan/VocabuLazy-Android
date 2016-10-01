f7d15aa420a928bad64d293e53200ad2a9481a38
allencheng07 <allencheng07@hotmail.com>
2016-10-02 03:06:26 +0800

    refine cover dialog

--- app/src/main/java/com/wishcan/www/vocabulazy/cover/activity/CoverActivity.java
+++ app/src/main/java/com/wishcan/www/vocabulazy/cover/activity/CoverActivity.java
@@ -1,73 +1,70 @@
 package com.wishcan.www.vocabulazy.cover.activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.AsyncTask;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
-import android.util.Log;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AlertDialog;
 
 import com.wishcan.www.vocabulazy.R;
-import com.wishcan.www.vocabulazy.cover.fragment.CoverDialogFragment;
 import com.wishcan.www.vocabulazy.mainmenu.activity.MainMenuActivity;
 import com.wishcan.www.vocabulazy.storage.Database;
+import com.wishcan.www.vocabulazy.utility.Logger;
 
-public class CoverActivity extends FragmentActivity implements CoverDialogFragment.OnDialogClickListener {
+public class CoverActivity extends FragmentActivity {
 
+    // TAG for debugging
     public static final String TAG = "CoverActivity";
 
-    public static final int VIEW_MAIN_RES_ID = R.id.activity_cover_container;
+    // layour reource ids
     private static final int VIEW_ACTIVITY_RES_ID = R.layout.activity_cover;
+
+    // google text-to-speech package name
     private static final String PACKAGE_NAME_GOOGLE_TTS_ENGINE = "com.google.android.tts";
 
-    private boolean isEngineInstalled;
+
+    /** Life cycles **/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(VIEW_ACTIVITY_RES_ID);
-
-        // check whether Google TTS engine has been installed
-        isEngineInstalled = checkAppInstalledOrNot(PACKAGE_NAME_GOOGLE_TTS_ENGINE);
-
-        // use AsyncTask to load database, prevent UI thread from being held
-        loadDatabase();
     }
 
     @Override
     protected void onResume() {
-        Log.d(TAG, "Resume");
         super.onResume();
 
-        // if the tts engine is not installed, launch the dialoag asking user to download and
-        // install Google Text-to-Speech Engine
-//        if (!isEngineInstalled) {
-//            Log.d(TAG, "launching cover dialog");
-//            CoverDialogFragment fragment = new CoverDialogFragment();
-//            fragment.addOnDialogClickListener(this);
-//            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-//            fragmentTransaction.add(VIEW_MAIN_RES_ID, fragment, "CoverDialogFragment");
-//            fragmentTransaction.commit();
-//        }
+        // check whether Text-to-Speech Engine is installed
+        checkTTSEngine();
     }
 
-    @Override
-    public void onYesClicked() {
-        directToGooglePlay(PACKAGE_NAME_GOOGLE_TTS_ENGINE);
-    }
+    /** Private methods **/
 
-    @Override
-    public void onNoClicked() {
-        directToVocabuLazy();
+    private void checkTTSEngine() {
+        try {
+
+            // load packages and check google tts engine
+            getPackageManager().getPackageInfo(PACKAGE_NAME_GOOGLE_TTS_ENGINE, PackageManager.GET_ACTIVITIES);
+
+            // if no exception happens, means tts engine is installed, then load the database
+            loadDatabase();
+        } catch (PackageManager.NameNotFoundException e) {
+
+            // exception caught means google tts engine is not installed,
+            // pop alert dialog and ask user to install tts engine
+            askUserToInstallTTS();
+        }
     }
 
     private void loadDatabase() {
         new AsyncTask<Void, Void, Void>() {
             @Override
             protected Void doInBackground(Void... voids) {
-                Log.d(TAG, "start loading database");
+                Logger.d(TAG, "Start loading database");
                 Database database = Database.getInstance();
                 database.loadFiles(getApplicationContext());
                 return null;
@@ -75,39 +72,46 @@ public class CoverActivity extends FragmentActivity implements CoverDialogFragme
 
             @Override
             protected void onPostExecute(Void aVoid) {
-                Log.d(TAG, "finish loading database");
+                Logger.d(TAG, "Finish loading database");
                 super.onPostExecute(aVoid);
-                if (isEngineInstalled) {
-                    directToVocabuLazy();
-
-                } else {
-                    CoverDialogFragment fragment = new CoverDialogFragment();
-                    fragment.addOnDialogClickListener(CoverActivity.this);
-                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                    fragmentTransaction.add(VIEW_MAIN_RES_ID, fragment, "CoverDialogFragment");
-                    fragmentTransaction.commit();
-                }
+                navigateToMainMenu();
             }
         }.execute();
     }
 
-    private boolean checkAppInstalledOrNot(String appUri) {
-        boolean isAppInstalled;
-        try {
-            getPackageManager().getPackageInfo(appUri, PackageManager.GET_ACTIVITIES);
-            isAppInstalled = true;
-        } catch (PackageManager.NameNotFoundException e) {
-            isAppInstalled = false;
-        }
-        return isAppInstalled;
+    private void askUserToInstallTTS() {
+        new AlertDialog.Builder(CoverActivity.this)
+                .setTitle(getResources().getString(R.string.cover_dialog_title))
+                .setMessage(getResources().getString(R.string.cover_dialog_message))
+                .setPositiveButton(getResources().getString(R.string.cover_dialog_button_confirm), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        navigateToGooglePlay(PACKAGE_NAME_GOOGLE_TTS_ENGINE);
+                    }
+                })
+                .setNegativeButton(getResources().getString(R.string.cover_dialog_button_cancel), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        loadDatabase();
+                    }
+                })
+                .setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        // do nothing
+                    }
+                })
+                .setCancelable(false)
+                .create()
+                .show();
     }
 
-    private void directToVocabuLazy() {
+    private void navigateToMainMenu() {
         startActivity(new Intent(CoverActivity.this, MainMenuActivity.class));
         finish();
     }
 
-    private void directToGooglePlay(String appPackageName) {
+    private void navigateToGooglePlay(String appPackageName) {
         try {
             startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
         } catch (android.content.ActivityNotFoundException anfe) {
