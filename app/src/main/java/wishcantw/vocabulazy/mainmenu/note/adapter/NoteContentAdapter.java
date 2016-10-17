cad865aebaa86955ceaafd06111bb2d35c1a4590
AllenCheng <allencheng07@hotmail.com>
2016-10-17 17:05:44 +0800

    Note null pointer prevented

--- app/src/main/java/wishcantw/vocabulazy/mainmenu/note/adapter/NoteContentAdapter.java
+++ app/src/main/java/wishcantw/vocabulazy/mainmenu/note/adapter/NoteContentAdapter.java
@@ -2,6 +2,7 @@ package wishcantw.vocabulazy.mainmenu.note.adapter;
 
 import android.content.Context;
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,19 +18,16 @@ import java.util.HashMap;
 
 public class NoteContentAdapter extends BaseExpandableListAdapter {
 
-    private int groupHeight;
-    private int childHeight;
-
     private Context mContext;
     private ArrayList<NoteExpandableGroupItem> mGroupItems;
     private HashMap<NoteExpandableGroupItem, ArrayList<NoteExpandableChildItem>> mChildItemsMap;
 
-    public NoteContentAdapter(Context context, ArrayList<NoteExpandableGroupItem> groupItems, HashMap<NoteExpandableGroupItem, ArrayList<NoteExpandableChildItem>> childItemsMap) {
+    public NoteContentAdapter(@NonNull Context context,
+                              @NonNull ArrayList<NoteExpandableGroupItem> groupItems,
+                              @NonNull HashMap<NoteExpandableGroupItem, ArrayList<NoteExpandableChildItem>> childItemsMap) {
         mContext = context;
         mGroupItems = groupItems;
         mChildItemsMap = childItemsMap;
-
-        calculateLayoutParams();
     }
 
     @Override
@@ -69,48 +67,56 @@ public class NoteContentAdapter extends BaseExpandableListAdapter {
 
     @Override
     public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
-        String groupIndex = "" + (groupPosition+1);
+
+        // get note strings
+        String groupIndex = String.valueOf(groupPosition+1);
         String groupTitle = ((NoteExpandableGroupItem) getGroup(groupPosition)).getGroupStr();
+
+        // if convert view is null, inflate a new one
         if (convertView == null) {
-            LayoutInflater infalInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = infalInflater.inflate(R.layout.item_note_group, parent, false);
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.item_note_group, parent, false);
         }
 
-        TextView groupIndexTextView = (TextView) convertView.findViewById(R.id.note_group_index);
-        groupIndexTextView.setText(groupIndex);
-        groupIndexTextView.setTypeface(null, Typeface.BOLD);
+        // set note index
+        ((TextView) convertView.findViewById(R.id.textView_index)).setText(groupIndex);
 
-        TextView groupTextView = (TextView) convertView.findViewById(R.id.note_group_text);
-        groupTextView.setText(groupTitle);
-//        groupTextView.setTypeface(null, Typeface.BOLD);
-//        groupTextView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, groupHeight));
+        // set note title
+        ((TextView) convertView.findViewById(R.id.textView_title)).setText(groupTitle);
 
-        ExpandableListView expandableListView = (ExpandableListView) parent;
-        boolean isLastGroup = (expandableListView.getExpandableListAdapter().getGroupCount()-1 == groupPosition);
+        // tag the last group
+        boolean isLastGroup = getGroupCount()-1 == groupPosition;
 
-        View groupDivider = convertView.findViewById(R.id.group_divider);
-        groupDivider.setVisibility((isExpanded || isLastGroup) ? View.GONE : View.VISIBLE);
+        // set divider visibility
+        convertView.findViewById(R.id.view_divider)
+                .setVisibility((isExpanded || isLastGroup)
+                        ? View.GONE
+                        : View.VISIBLE);
 
         return convertView;
     }
 
     @Override
     public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+
+        // get title strings
         String childTitle = ((NoteExpandableChildItem) getChild(groupPosition, childPosition)).getChildStr();
+
+        // if the convert view is null, inflate one
         if (convertView == null) {
-            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inflater.inflate(R.layout.item_note_child, parent, false);
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.item_note_child, parent, false);
         }
 
-        TextView childTextView = (TextView) convertView.findViewById(R.id.note_child_text);
-        childTextView.setText(childTitle);
-//        childTextView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, childHeight));
+        // set note child text
+        ((TextView) convertView.findViewById(R.id.textView_option)).setText(childTitle);
 
-        ExpandableListView expandableListView = (ExpandableListView) parent;
-        boolean isLastGroup = (expandableListView.getExpandableListAdapter().getGroupCount()-1 == groupPosition);
+        // tag for last group
+        boolean isLastGroup = getGroupCount()-1 == groupPosition;
 
-        View childDivider = convertView.findViewById(R.id.child_divider);
-        childDivider.setVisibility((isLastChild && !isLastGroup) ? View.VISIBLE : View.GONE);
+        // set divider visibility
+        convertView.findViewById(R.id.view_divider)
+                .setVisibility((isLastChild && !isLastGroup)
+                        ? View.VISIBLE
+                        : View.GONE);
 
         return convertView;
     }
@@ -118,17 +124,5 @@ public class NoteContentAdapter extends BaseExpandableListAdapter {
     @Override
     public boolean isChildSelectable(int groupPosition, int childPosition) {
         return true;
-    }
-
-    private void calculateLayoutParams() {
-        DisplayMetrics displayMetrics = mContext.getResources().getDisplayMetrics();
-        float screenHeightDp = displayMetrics.heightPixels / displayMetrics.density;
-        int groupHeightDp = (int) (screenHeightDp/10 + 0.5f);
-        groupHeight = toPixel(groupHeightDp, mContext.getResources().getDisplayMetrics().density);
-        childHeight = groupHeight;
-    }
-
-    private int toPixel(int dp, float density) {
-        return (int) (dp*density + 0.5f);
     }
 }
